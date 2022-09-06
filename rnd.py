c1b203db6f3bcd457930f09e444a353cf7139d2f
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2022-09-06 01:15:10 -0300

    Add last_checked stat; fix URL gen.; JPG previews
    – URLs now won't generate starting or ending with "-"
    – URLs now won't generate with sequential "-"s

--- rnd.py
+++ rnd.py
@@ -2,12 +2,17 @@ import string
 import random
 
 # Alphabet minus l
-ALPHABET = "abcdefghijkmnopqrstuvwxyz" + string.digits + '_'
+ALPHANUM_HYPH = "abcdefghijkmnopqrstuvwxyz" + string.digits + '-'
+ALPHANUM = "abcdefghijkmnopqrstuvwxyz" + string.digits
 N = 5
 
 # [Ref] stackoverflow.com/a/2257449/4824627
 def genID():
-  return "".join(random.choice(ALPHABET) for _ in range(N))
+  return "".join([
+    random.choice(ALPHANUM),
+    "".join(random.choice(ALPHANUM_HYPH) for _ in range(N-2)),
+    random.choice(ALPHANUM)
+  ]).replace("--", "-" + random.choice(ALPHANUM))
 
 while True:
   print(genID(), end="")
